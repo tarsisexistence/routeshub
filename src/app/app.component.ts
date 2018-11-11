bbb8ed73d725b8130eee08dbf2d3a2d29116ec15
maktarsis <tarsis.maksym@gmail.com>
2018-11-11 10:17:58 +0200

    refactor(example): actualize, set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'example-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'routeshub';
+  public title = 'Routeshub';
 }
