21d9f6f470428f1c2ed702e903b1290323c9b08f
maktarsis <tarsis.maksym@gmail.com>
2019-01-06 17:37:28 +0200

    chore(project): add snyk, add snyk and lint scripts, refactor some stuff

--- package/test/karma.conf.js
+++ package/test/karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function(config) {
   config.set({
     basePath: '../src',
@@ -13,7 +10,7 @@ module.exports = function(config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../coverage'),
