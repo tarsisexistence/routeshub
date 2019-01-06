7ced3400605439afb50df796a32015a5b3cd831d
maktarsis <tarsis.maksym@gmail.com>
2019-01-06 15:43:19 +0200

    refactor(project): fix tslint

--- integration/test.ts
+++ integration/test.ts
@@ -1,5 +1,3 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
@@ -9,12 +7,11 @@ import {
 
 declare const require: any;
 
-// First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
-// Then we find all the tests.
+
 const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
+
 context.keys().map(context);
