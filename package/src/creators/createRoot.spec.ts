d5e23bf8df36df537191a4d93250ea769a52ec59
maktarsis <tarsis.maksym@gmail.com>
2019-03-09 10:15:06 +0200

    chore(test): add new test, add more configurations

--- package/src/creators/createRoot.spec.ts
+++ package/src/creators/createRoot.spec.ts
@@ -1,6 +1,7 @@
 import { createRoot } from './createRoot';
 import { reset } from '../utils/reset';
 
+/* tslint:disable:max-line-length */
 describe('createRoot', () => {
   afterEach(() => {
     reset();
@@ -21,6 +22,48 @@ describe('createRoot', () => {
         children: null
       }
     };
-    expect(JSON.stringify(appSlice)).toEqual(JSON.stringify(result));
+    expect(JSON.stringify(appSlice)).toBe(JSON.stringify(result));
+  });
+
+  it('should create a simple hub with a few routes', () => {
+    const notes = {
+      root: { path: '' },
+      notFound: { path: '**' },
+      map: { path: 'map' }
+    };
+    const appSlice: any = createRoot(notes);
+    const result = {
+      root: {
+        id: 0,
+        parentId: null,
+        state: ['/'],
+        stateFn: Function,
+        path: '',
+        lazy: null,
+        routeName: 'root',
+        children: null
+      },
+      notFound: {
+        id: 1,
+        parentId: null,
+        state: ['**'],
+        stateFn: Function,
+        path: '**',
+        lazy: null,
+        routeName: 'notFound',
+        children: null
+      },
+      map: {
+        id: 2,
+        parentId: null,
+        state: ['/', 'map'],
+        stateFn: Function,
+        path: 'map',
+        lazy: null,
+        routeName: 'map',
+        children: null
+      }
+    };
+    expect(JSON.stringify(appSlice)).toBe(JSON.stringify(result));
   });
 });
