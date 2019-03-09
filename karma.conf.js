55bd9ddb901aa5627ea40f639e4d836884b32afb
maktarsis <tarsis.maksym@gmail.com>
2019-03-09 07:34:55 +0200

    feat(deps): add karma-parallel package

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,19 @@
 module.exports = function(config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['parallel', 'jasmine', '@angular-devkit/build-angular'],
     plugins: [
+      require('karma-parallel'),
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    parallelOptions: {
+      executors: 1,
+      shardStrategy: 'round-robin'
+    },
     client: {
       clearContext: false
     },
@@ -22,7 +27,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
