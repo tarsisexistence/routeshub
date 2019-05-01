0a7aaa54ebc8ee89d311d7d133537c745efc45b3
maktarsis <tarsis.maksym@gmail.com>
2019-05-01 15:21:21 +0300

    fix(build): fix warning due to refreshChildren

--- package/src/hub.ts
+++ package/src/hub.ts
@@ -13,10 +13,14 @@ export const hub: BehaviorSubject<Hub<any>> = new BehaviorSubject(null);
  */
 const entitify = <R, C>(routes: Slice<R>): Slice<R, C> =>
   Object.keys(routes).reduce(
-    (acc: Slice<R, C>, routeName: string): Slice<R, C> =>
-      routes[routeName].children
-        ? Object.assign({}, acc, refreshChildren<R, C>(routes[routeName]))
-        : Object.assign({}, acc, { [routeName]: routes[routeName] }),
+    (acc: Slice<R, C>, routeName: string): Slice<R, C> => {
+      if (!routes[routeName].children) {
+        return Object.assign({}, acc, { [routeName]: routes[routeName] });
+      }
+
+      const refreshedChildren = refreshChildren<R, C>(routes[routeName]);
+      return Object.assign({}, acc, refreshedChildren);
+    },
     {} as Slice<R, C>
   );
 
