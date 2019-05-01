633146524629f393e34238f9e5778bfeeccd5b62
maktarsis <tarsis.maksym@gmail.com>
2019-05-01 20:44:45 +0300

    feat(lib): add forwardParams as public api

--- package/src/public_api.ts
+++ package/src/public_api.ts
@@ -1,5 +1,7 @@
 export { rootNote } from './generators';
 
+export { forwardParams } from './utils/state';
+
 export { createRoot, createFeature } from './creators';
 
 export { RootRoute, RouteNote, RoutesNotes, Slice, Slices } from './interfaces';
