e187562b4849528f3d044731faba12a0bef8a4a0
maktarsis <tarsis.maksym@gmail.com>
2019-06-19 23:38:51 +0300

    test(lib): add unit test for navLink directive

---
+++ package/src/navigation/navigation.spec.ts
@@ -0,0 +1,97 @@
+// tslint:disable:max-line-length
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+import {
+  async,
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick
+} from '@angular/core/testing';
+import { getHubSlices } from '../hub';
+import { NavigationModule } from './navigation.module';
+import { createRoot } from '../creators/root.creator';
+import { Routes } from '@angular/router';
+import { reset } from '../utils/reset';
+
+const APP_HUB_KEY = Symbol();
+
+@Component({
+  selector: 'app-header',
+  template: `
+    <a [navLink]="slices.app.root.state">Home</a>
+    <a navLink="{{ slices.app.about.state }}">About</a>
+    <a [navLink]="slices.app.map">Map</a>
+  `
+})
+class HeaderComponent {
+  public slices = getHubSlices();
+}
+
+const routes: Routes = ([] = [
+  {
+    path: '',
+    component: HeaderComponent
+  },
+  {
+    path: 'about',
+    component: HeaderComponent
+  },
+  {
+    path: 'map',
+    component: HeaderComponent
+  }
+]);
+
+describe('Navigation', () => {
+  let location: Location;
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeaderComponent],
+      imports: [NavigationModule, RouterTestingModule.withRoutes(routes)]
+    }).compileComponents();
+
+    createRoot(routes, APP_HUB_KEY);
+  }));
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    location = TestBed.get(Location);
+  });
+
+  afterEach(() => {
+    reset();
+  });
+
+  it('should have root path', fakeAsync(() => {
+    expect(location.path()).toBe('');
+  }));
+
+  it('should navigate and change location path', fakeAsync(() => {
+    const link = fixture.debugElement.nativeElement.querySelectorAll('a')[1];
+    link.click();
+    tick();
+    expect(location.path()).toBe('/about');
+  }));
+
+  it('should navigate and change location path and return', fakeAsync(() => {
+    const links = fixture.debugElement.nativeElement.querySelectorAll('a');
+    links[1].click();
+    tick();
+    links[0].click();
+    tick();
+    expect(location.path()).toBe('/');
+  }));
+
+  it('should navigate without state prop with square brackets', fakeAsync(() => {
+    const link = fixture.debugElement.nativeElement.querySelectorAll('a')[2];
+    link.click();
+    tick();
+    expect(location.path()).toBe('/map');
+  }));
+});
