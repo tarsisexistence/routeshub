b505ff6100dfd075d8828f422d3bbfd25ab9e21c
Denis Makarov <denis.makarov@piano.io>
2020-05-29 22:45:07 +0300

    feat(schematics): getting ast

--- package/schematics/routeshub-parse/index.ts
+++ package/schematics/routeshub-parse/index.ts
@@ -1,8 +1,44 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import * as ts from 'typescript';
+import { existsSync, readFileSync } from 'fs';
+import { dirname, resolve } from 'path';
+
+const getTsConfig = (content: any): ts.ParsedCommandLine => {
+  const parseConfigHost: ts.ParseConfigHost = {
+    fileExists: existsSync,
+    readDirectory: ts.sys.readDirectory,
+    readFile: file => readFileSync(file, 'utf8'),
+    useCaseSensitiveFileNames: true
+  };
+
+  return ts.parseJsonConfigFileContent(
+    content,
+    parseConfigHost,
+    resolve(dirname('tsconfig.json')),
+    {
+      noEmit: true
+    }
+  );
+};
+
+const getProgram = (tsConfigContent: string): ts.Program => {
+  const config = getTsConfig(tsConfigContent);
+  const host = ts.createCompilerHost(config.options, true);
+  return ts.createProgram(config.fileNames, config.options, host);
+};
 
 export function routeshubParse(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    console.log(_context, _options);
+    const config = tree.get('./tsconfig.json');
+    if (!config) {
+      throw new Error("tsconfig didn't find");
+    }
+
+    const content = config.content.toString();
+    const program = getProgram(JSON.parse(content));
+    const files = program.getSourceFiles();
+    console.log(files);
+
     return tree;
   };
 }
