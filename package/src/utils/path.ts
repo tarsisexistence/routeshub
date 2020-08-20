300c76d1185fc3b1ccc7604ee005fc199985591e
retarsis <tarsis.maksym@gmail.com>
2020-08-20 14:42:38 +0300

    test(path): add tests for few path utils

--- package/src/utils/path.ts
+++ package/src/utils/path.ts
@@ -1,20 +1,15 @@
 import { DefaultNameOptions } from '../interfaces';
 
 /**
- * removes empty or slash paths
- */
-export const setOnlyPaths = (link: string[]): string[] =>
-  link.filter((path: string) => path.length > 0 && path !== '/');
-
-/**
  * Prevents to record multi path in the state
  */
 export const transformPathToState = (
   path: string,
   state: string[] = ['/']
 ): string[] => {
-  const rawPaths = path.split('/');
-  const paths = setOnlyPaths(rawPaths);
+  const paths = path
+    .split('/')
+    .filter((path: string) => path.length > 0 && path !== '/');
   return state.concat(paths);
 };
 
