20abe3eb9f7f1d0d2d89a60e04ed43faa231eeb3
limitofzero <limitofzero@gmail.com>
2020-08-27 21:33:57 +0300

    chore(schematics): add test case

--- fixtures/test-app/src/app/app-routing.module.ts
+++ fixtures/test-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelpComponent } from './help.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'help', component: HelpComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- fixtures/test-app/src/app/app.module.ts
+++ fixtures/test-app/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelpComponent } from './help.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HelpComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
