3b06076e564f91062a97b43a934c345d1371159e
limitofzero <limitofzero@gmail.com>
2020-08-27 21:45:26 +0300

    chore(schematics): add missed files

--- fixtures/test-app/src/app/app.module.ts
+++ fixtures/test-app/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelpComponent } from './help.component';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
   declarations: [AppComponent, HelpComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, DashboardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- package/src/routelar/tests/parse-routes.spec.ts
+++ package/src/routelar/tests/parse-routes.spec.ts
@@ -2,23 +2,32 @@ import { resolve } from 'path';
 import { readFileSync } from 'fs';
 import { WorkspaceSchema } from '@angular-devkit/core/src/experimental/workspace';
 import { Project } from 'ts-morph';
+import { parseRoutes } from '../parse/parse-routes';
 
 describe('parseProject', () => {
   const PROJECT_NAME = 'test-app';
-  const pathRelativeAngularJSON = '../../../angular.json';
+  const pathRelativeAngularJSON = '../../../../angular.json';
   const pathToJSON = resolve(__dirname, pathRelativeAngularJSON);
   const content = JSON.parse(
     readFileSync(pathToJSON).toString()
   ) as WorkspaceSchema;
-  const relativePathToTS = '../../../fixtures/test-app/tsconfig.app.json';
+  const relativePathToTS = '../../../../fixtures/test-app/tsconfig.app.json';
   const pathToTS = resolve(__dirname, relativePathToTS);
 
   it('should be parse project', () => {
+    const expectedRouteMap: Routelar.Parse.RouteTree = {
+      root: {},
+      help: {}
+    };
+
     const project = new Project({
       tsConfigFilePath: pathToTS,
       addFilesFromTsConfig: true
     });
 
-    expect(project).toBeDefined();
+    const workspace = content.projects[PROJECT_NAME];
+
+    const routes = parseRoutes(workspace, project);
+    expect(routes).toEqual(expectedRouteMap);
   });
 });
