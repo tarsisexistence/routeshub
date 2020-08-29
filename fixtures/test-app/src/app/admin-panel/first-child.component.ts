4bc1599901d3f44ed73e9ef3360f9444bcd69828
limitofzero <limitofzero@gmail.com>
2020-08-29 20:14:41 +0300

    fix(schematics): fix component in test-app

--- fixtures/test-app/src/app/admin-panel/first-child.component.ts
+++ fixtures/test-app/src/app/admin-panel/first-child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-first-child',
@@ -8,4 +8,4 @@ import { Component, OnInit } from '@angular/core';
     </p>
   `
 })
-export class FirstChildComponent implements OnInit {}
+export class FirstChildComponent {}
