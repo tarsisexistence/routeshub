00502a03214509e4774d3055e30f2516389c5c21
retarsis <tarsis.maksym@gmail.com>
2020-08-29 15:42:08 +0300

    feat(routelar): add getRoutes API

--- package/src/routelar/core/getRoutes.test.ts
+++ package/src/routelar/core/getRoutes.test.ts
@@ -1,23 +1,25 @@
 // tslint:disable:max-line-length
-import { proxy } from './proxy';
+import { getRoutes } from './getRoutes';
+
+const routes = getRoutes<any>();
 
 describe('core', () => {
   describe('proxy', () => {
     describe('for output', () => {
       test('should return default path', () => {
-        expect(Array.from(proxy)).toEqual(['/']);
+        expect(Array.from(routes)).toEqual(['/']);
       });
 
       test('should return first path', () => {
-        expect(Array.from(proxy.one)).toEqual(['/', 'one']);
+        expect(Array.from(routes.one)).toEqual(['/', 'one']);
       });
 
       test('should return two paths', () => {
-        expect(Array.from(proxy.one.two)).toEqual(['/', 'one', 'two']);
+        expect(Array.from(routes.one.two)).toEqual(['/', 'one', 'two']);
       });
 
       test('should return three paths', () => {
-        expect(Array.from(proxy.one.two.three)).toEqual([
+        expect(Array.from(routes.one.two.three)).toEqual([
           '/',
           'one',
           'two',
@@ -26,7 +28,7 @@ describe('core', () => {
       });
 
       test('should return three paths when values duplicated', () => {
-        expect(Array.from(proxy.one.one.one)).toEqual([
+        expect(Array.from(routes.one.one.one)).toEqual([
           '/',
           'one',
           'one',
@@ -35,11 +37,11 @@ describe('core', () => {
       });
 
       test('should return path with possible Array.prototype.map', () => {
-        expect(Array.from(proxy.one.map)).toEqual(['/', 'one', 'map']);
+        expect(Array.from(routes.one.map)).toEqual(['/', 'one', 'map']);
       });
 
       test('should return path with prop of possible Array.prototype.map', () => {
-        expect(Array.from(proxy.one.map.notMap)).toEqual([
+        expect(Array.from(routes.one.map.notMap)).toEqual([
           '/',
           'one',
           'map',
@@ -50,19 +52,19 @@ describe('core', () => {
 
     describe('for type', () => {
       test('should return result of array', () => {
-        expect(Array.isArray(proxy)).toBeTruthy();
+        expect(Array.isArray(routes)).toBeTruthy();
       });
 
       test('should return nested result of array', () => {
-        expect(Array.isArray(proxy.somePath)).toBeTruthy();
+        expect(Array.isArray(routes.somePath)).toBeTruthy();
       });
 
       test('should return nested not Array.prototype.map result of array', () => {
-        expect(Array.isArray(proxy.map)).toBeTruthy();
+        expect(Array.isArray(routes.map)).toBeTruthy();
       });
 
       test('should return nested prop of not Array.prototype.map result of array', () => {
-        expect(Array.isArray(proxy.map.someProp)).toBeTruthy();
+        expect(Array.isArray(routes.map.someProp)).toBeTruthy();
       });
     });
   });
