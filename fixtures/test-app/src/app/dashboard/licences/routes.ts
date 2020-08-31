49f8d1946b6019198763f1cb9b4ee075d620c8bf
retarsis <tarsis.maksym@gmail.com>
2020-08-31 14:22:01 +0300

    chore(routelar): migrated to https://github.com/retarsis/routerkit

--- fixtures/test-app/src/app/dashboard/licences/routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { LicenseComponent } from './license.component';
-
-export const routes: Routes = [
-  { path: '', component: LicenseComponent }
-];
--- package/src/routelar/generation/constants.ts
+++
@@ -1,3 +0,0 @@
-export const STRING_KEYWORD = 'string';
-
-export const TYPES_FILENAME = 'routelar.d.ts';
